ffcacccb134b5e6043fb4e7e68dc52ae1865dfdd
jasfoley <k00186690@student.lit.ie>
2017-12-04 18:50:14 +0000

    Game Commit
    Game commit for GitHub

---
+++ DeathDrive/Assets/DeathDrive/Scripts/EnemyHealth.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public int scoreValue = 10;
+    public AudioClip deathClip;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    EnemyMovement enemyMovement;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        enemyMovement = GetComponent<EnemyMovement>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        currentHealth = startingHealth;
+    }
+
+
+
+    public void TakeDamage(int amount)
+    {
+
+        damaged = true;
+
+        currentHealth -= amount;
+
+
+        enemyAudio.Play();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+
+        enemyMovement.enabled = false;
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        Destroy(gameObject, 2f);
+        //ScoreManager.score += scoreValue;
+    }
+
+}
---
+++ DeathDrive/Assets/DeathDrive/Scripts/PlayerColider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColider : MonoBehaviour {
+
+    public int attackDamage = 100;
+
+    Animator anim;
+    GameObject enemy;
+    EnemyHealth enemyHealth;
+    bool enemyInRange;
+    float timer;
+
+    void Awake()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyHealth = enemy.GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject == enemy)
+        {
+            enemyInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if (other.gameObject == enemy)
+        {
+            enemyInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (enemyInRange)
+        {
+            Attack();
+        }
+    }
+
+
+    void Attack()
+    {
+
+        if (enemyHealth.currentHealth > 0)
+        {
+            enemyHealth.TakeDamage(attackDamage);
+        }
+    }
+
+}
